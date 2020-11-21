f5c4163f10d3a21e7ca9618bf48a3238542214d4
papukweh <nathalia.borin@usp.br>
2020-11-21 19:40:37 -0300

    Improves front end design, adds option to download MIDI

--- backend/app.py
+++ backend/app.py
@@ -6,9 +6,29 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+BASE_PATH = 'user_files/'
+
 @app.route('/')
 def main_page():  
   return 'Please use the /convert route for automatic music transcription'
+
+@app.route('/get_midi/<filename>', methods=['GET'])
+def get_midi(filename):
+  filename = filename if filename else 'input_song.mid'
+  midi_path = f'{BASE_PATH}{filename}'
+  try:
+    return send_file(midi_path, mimetype='audio/midi')
+  except:
+    abort(404)
+
+@app.route('/get_pdf/<filename>', methods=['GET'])
+def get_pdf(filename):
+  filename = filename if filename else 'input_song.pdf'
+  pdf_path = f'{BASE_PATH}{filename}'
+  try:
+    return send_file(pdf_path, mimetype='application/pdf')
+  except:
+    abort(404)
 
 @app.route('/convert', methods=['POST'])
 def convert():
@@ -18,10 +38,11 @@ def convert():
   minduration = request.args.get('minduration', default=0.1, type=float)
   confidence = request.args.get('confidence', default=0.9, type=float)
   output_format = request.args.get('format', default='pdf', type=str)
+  filename = request.args.get('filename', default='input_song', type=str)
 
   # Gets audio file and saves
   song = request.files['file']
-  path = 'user_files/input_song'
+  path = f'{BASE_PATH}{filename}'
   song.save(path)
   
   # Converts to MIDI
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,34 @@
 import { useState } from 'react'
 import MicRecorder from 'mic-recorder-to-mp3'
 import axios from 'axios'
+import { v4 } from 'uuid'
 import { Document, Page, pdfjs } from 'react-pdf'
 import { saveAs } from 'file-saver'
+import { Button } from '@material-ui/core'
+import StopIcon from '@material-ui/icons/Stop'
+import RecordIcon from '@material-ui/icons/Mic'
+import PlayIcon from '@material-ui/icons/PlayArrow'
+import PauseIcon from '@material-ui/icons/Pause'
+import SaveIcon from '@material-ui/icons/Save'
+import MidiIcon from '@material-ui/icons/MusicNote'
+
+import './App.css'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 const recorder = new MicRecorder({
-  bitRate: 128
+  bitRate: 320
 })
 
 export default function App () {
   const [isRecording, setRecording] = useState(false)
+  const [isPlaying, setPlaying] = useState(false)
+  const [isProcessing, setProcessing] = useState(false)
   const [audio, setAudio] = useState(null)
+  const [midi, setMidi] = useState(null)
   const [score, setScore] = useState(null)
 
-  const onRecordButtonClick = () => {
+  const onStartRecordingButtonClick = () => {
     setRecording(true)
     recorder.start().then(() => {
       console.log('Recording!!')
@@ -25,7 +38,7 @@ export default function App () {
       })
   }
 
-  const onStopButtonClick = () => {
+  const onStopRecordingButtonClick = () => {
     setRecording(false)
     recorder.stop().getMp3().then(([buffer, blob]) => {
       console.log('Finished recording!')
@@ -38,14 +51,20 @@ export default function App () {
         type: blob.type,
         lastModified: Date.now()
       })
-      setAudio(file)
+      setAudio(new Audio(URL.createObjectURL(file), { onEnded: onStopButtonClick }))
       form.append('file', file)
-      axios.post('http://localhost:5000/convert', form, headers)
+      setProcessing(true)
+      const filename = v4()
+      axios.post(`http://localhost:5000/convert?filename=${filename}`, form, headers)
         .then((res) => {
-          console.log(res)
           const blob = new Blob([res.data], { type: 'application/pdf' })
-          console.log(blob)
           setScore(URL.createObjectURL(blob))
+          axios.get(`http://localhost:5000/get_midi/${filename}`, headers)
+            .then((res) => {
+              const blob = new Blob([res.data], { type: 'audio/midi' })
+              setMidi(URL.createObjectURL(blob))
+              setProcessing(false)
+            })
         })
         .catch((err) => console.log(err))
     })
@@ -55,15 +74,34 @@ export default function App () {
       })
   }
 
+  const onStopButtonClick = () => {
+    if (audio && isPlaying) {
+      setPlaying(false)
+      audio.pause()
+      audio.currentTime = 0
+    }
+  }
+
+  const onPauseButtonClick = () => {
+    if (audio && isPlaying) {
+      setPlaying(false)
+      audio.pause()
+    }
+  }
+
   const onPlayButtonClick = () => {
     if (audio) {
-      const player = new Audio(URL.createObjectURL(audio))
-      player.play()
+      setPlaying(true)
+      audio.play()
     }
   }
 
   const saveScore = () => {
     saveAs(score, 'score.pdf')
+  }
+
+  const saveMidi = () => {
+    saveAs(midi, 'score.mid')
   }
 
   const [numPages, setNumPages] = useState(null)
@@ -75,23 +113,38 @@ export default function App () {
   }
 
   return (
-    <div>
-      { isRecording
-        ? (<button onClick={onStopButtonClick}>Stop Recording</button>)
-        : (<button onClick={onRecordButtonClick}>Start Recording</button>)
-      }
-      <button onClick={onPlayButtonClick}>Play</button>
-      { score
-        ? (
-            <div>
-              <button onClick={saveScore}>Save PDF</button>
-              <Document file={score} onLoadSuccess={onDocumentLoadSuccess}>
-              <Page pageNumber={pageNumber} />
-              </Document>
-              <p>Page {pageNumber} of {numPages}</p>
-            </div>
-          )
-        : null }
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Melody Snippets</h1>
+      </header>
+      <body className="App-body">
+        { isRecording
+          ? (<Button variant="contained" color="secondary" onClick={onStopRecordingButtonClick} startIcon={<StopIcon />}>Stop Recording</Button>)
+          : (<Button variant="contained" color="primary" onClick={onStartRecordingButtonClick} startIcon={<RecordIcon />}>Start Recording</Button>)
+        }
+        <div>
+          <Button variant="contained" color="default" onClick={onPlayButtonClick} startIcon={<PlayIcon />}></Button>
+          <Button variant="contained" color="default" onClick={onPauseButtonClick} startIcon={<PauseIcon />}></Button>
+          <Button variant="contained" color="default" onClick={onStopButtonClick} startIcon={<StopIcon />}></Button>
+        </div>
+        {
+          isProcessing
+            ? (<span>Processing...</span>)
+            : null
+        }
+        { score
+          ? (
+              <div>
+                <Button variant="contained" color="primary" onClick={saveScore} startIcon={<SaveIcon />}>Save PDF</Button>
+                <Button variant="contained" color="primary" onClick={saveMidi} startIcon={<MidiIcon />}>Save MIDI</Button>
+                <Document file={score} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
+                </Document>
+                <p>Page {pageNumber} of {numPages}</p>
+              </div>
+            )
+          : null }
+      </body>
+    </div>
   )
 }
