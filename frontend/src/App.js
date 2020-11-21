edf34dfb5b1ce8273907c50af4ce2b2d098800b8
papukweh <nathalia.borin@usp.br>
2020-11-20 22:45:18 -0300

    Adds CORS to backend, simple frontend with record button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import MicRecorder from 'mic-recorder-to-mp3'
+import axios from 'axios'
 
-function App() {
+const recorder = new MicRecorder({
+  bitRate: 128
+})
+
+export default function App () {
+  const [isRecording, setRecording] = useState(false)
+
+  const onClick = () => {
+    if (isRecording) {
+      setRecording(false)
+      recorder.stop().getMp3().then(([buffer, blob]) => {
+        console.log('Finished recording!')
+        const form = new FormData()
+        const headers = {
+          'Content-Type': 'multipart/form-data'
+        }
+        const file = new File(buffer, 'audio.mp3', {
+          type: blob.type,
+          lastModified: Date.now()
+        })
+        form.append('file', file)
+        axios.post('http://localhost:5000/convert', form, headers)
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err))
+        const player = new Audio(URL.createObjectURL(file))
+        player.play()
+      })
+      .catch((e) => {
+        alert('We could not retrieve your message')
+        console.log(e)
+      })
+    } else {
+      setRecording(true)
+      recorder.start().then(() => {
+        console.log('Recording!!')
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onClick}>Record</button>
     </div>
-  );
+  )
 }
-
-export default App;
